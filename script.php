45c27d929b66066c7c3ac5852e688025bd8af6cb
Jefter-Rafael <jefterrafael1.1@gmail.com>
2023-05-08 18:33:36 -0300

    first commit

---
+++ script.php
@@ -0,0 +1,22 @@
+<?php
+
+function create($name)
+{
+    // Nome do arquivo a ser criado
+    $arquivo = './'.$name . ".json";
+    // Texto a ser escrito no arquivo
+    $texto =
+        file_get_contents('https://cpback.agreablecode.com/api/' . $name);
+    // Cria o arquivo ou abre para escrita (modo "w")
+    $handle = fopen($arquivo, "w");
+    // Escreve o texto no arquivo
+    fwrite($handle, $texto);
+    // Fecha o arquivo
+    fclose($handle);
+}
+$array = ['gpu','memory','powersuply','ssds','processorInfo','motherboard', 'processors', 'cases'];
+foreach ($array as $key => $value) {
+    create($value);
+}
+
+?>
